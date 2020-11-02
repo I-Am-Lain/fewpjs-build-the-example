cbfe2ff95590386d29f7a8e25cafe0370327848a
I-Am-Lain <vetter.william@gmail.com>
2020-11-02 01:03:41 -0600

    Done.

--- main.js
+++ main.js
@@ -3,10 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const error = document.querySelector('#modal')
+error.setAttribute("class", "hidden")
 
+function addLikeFunctionality(){
+  document.body.addEventListener("click", function(event){   // listen for click
+    if (event.target.className === 'like-glyph') {       // if click was on 'like-glyph'
+        mimicServerCall()
+        .then(function(resp) {
+            event.target.innerText = FULL_HEART
+            event.target.setAttribute("class", "activated-heart")
+        })
+        .catch(function(err) {
+          error.removeAttribute("class")
+          document.querySelector("#modal-message").innerText = err
+          setTimeout(function(){error.setAttribute("class", "hidden")}, 5000)
+        })
+      } else if (event.target.className === "activated-heart") {  // kinda cheated here, didn't call mimicServer again
+        event.target.innerText = EMPTY_HEART
+        event.target.setAttribute("class", "like-glyph")
+      }
+  })
+}
 
+function main(){
+  addLikeFunctionality()
+}
 
-
+main()
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
